db2a5fd4c12a38515bbece788332864cd7227b83
rishu7866 <iamrishu.3000@gmail.com>
2023-04-04 20:28:44 +0530

    in app.js 3/04

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
-app.use("/",postRouter)
+app.use("/postview",postRouter)
 
 
 app.listen(process.env.PORT || 8000,()=>{
